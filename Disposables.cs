c2698686a281eaed839eebd3d3218969de44352e
Tony <caddzooks@gmail.com>
2024-02-06 18:38:51 -0500

    Add files via upload

---
+++ Disposables.cs
@@ -0,0 +1,176 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace MyNamespace
+{
+
+   /// <summary>
+   /// A class that manages the disposal of an ordered
+   /// set of IDisposable instances.
+   /// 
+   /// Contract: IDisposable instances managed by this
+   /// class are disposed in the reverse order in which 
+   /// they are added.
+   /// 
+   /// </summary>
+
+   public static class Disposables
+   {
+      static object lockObj = new object();
+      static OrderedSet<IDisposable> list = new OrderedSet<IDisposable>();
+
+      static Disposables()
+      {
+         Application.QuitWillStart += quitWillStart;
+      }
+
+      private static void quitWillStart(object sender, EventArgs e)
+      {
+         Clear(true);  
+      }
+
+      /// <summary>
+      /// This method can be passed any number of IDisposables.
+      /// When the Clear() method is called, the elements will
+      /// be disposed.
+      /// 
+      /// e.g.:
+      /// 
+      ///    Circle circle = new Circle(Point3d.Origin, Vector3d.ZAxis, 1.0);
+      ///    Disposables.Add(circle);
+      ///    
+      /// Note that in lieu of calling Add(), the AutoDispose() extension
+      /// method can be used thusly:
+      ///
+      ///    Circle circle = new Circle(Point3d.Origin, Vector3d.ZAxis, 1.0).AutoDispose();
+      ///    
+      /// </summary>
+      /// <param name="disposable">The item to be disposed at shutdown
+      /// or when the Clear() method is called</param>
+      /// <param name="add">troe to add the item, false to remove it</param>
+      /// 
+
+      public static void Add(params IDisposable[] args)
+      {
+         if(args == null)
+            throw new ArgumentNullException(nameof(args));
+         lock(lockObj)
+         {
+            foreach(IDisposable disposable in args)
+            {
+               if(disposable == null)
+                  throw new ArgumentException("null element");
+               if(!list.Contains(disposable))
+                  list.Add(disposable);
+            }
+         }
+      }
+
+      /// <summary>
+      /// Removes an IDisposable that is queued for 
+      /// disposiong without disposing it.
+      /// </summary>
+
+      public static bool Remove(IDisposable disposable)
+      {
+         lock(lockObj)
+         {
+            return list.Remove(disposable);
+         }
+      }
+
+      /// <summary>
+      /// Checks if a given IDisposable is already queued for disposing:
+      /// </summary>
+
+      public static bool Contains(IDisposable disposable) => list.Contains(disposable);
+
+      /// <summary>
+      /// The count of elements currently queued for disposal:
+      /// </summary>
+      
+      public static int Count => list.Count;
+
+      /// <summary>
+      /// Disposes all elements queued for disposal. This method
+      /// is autonomously called when AutoCAD shuts down, so there
+      /// is no need to explicitly call it at shutdown. 
+      /// 
+      /// This method can also be called at any time to dispose and 
+      /// dequeue IDisposable instances that were previously-queued
+      /// using the Add() method.
+      /// </summary>
+
+      public static void Clear()
+      {
+         Clear(false);
+      }
+
+      static void Clear(bool terminating = false)
+      {
+         System.Exception exception = null;
+         lock(lockObj)
+         {
+            for(int i = list.Count - 1; i > -1; i--)
+            {
+               try
+               {
+                  IDisposable item = list[i];
+                  if(item != null)
+                  {
+                     DisposableWrapper wrapper = item as DisposableWrapper;
+                     if(wrapper != null && wrapper.IsDisposed)
+                        continue;
+                     item.Dispose();
+                  }
+               }
+               catch(System.Exception ex)
+               {
+                  exception = exception ?? ex;
+               }
+            }
+            list.Clear();
+         }
+         if(exception != null)
+         {
+            if(!terminating)
+               throw exception;
+            else
+            {
+               Debug.WriteLine($"Dispose() threw {exception}");
+               Console.Beep();
+            }
+         }
+      }
+
+      /// <summary>
+      /// An extension method targeting IDisposable, that can be used
+      /// in lieu of the Add() method. If the add argument is true, the
+      /// target is queued for disposal. If the add argument is false
+      /// the target is dequeued for disposal and is not disposed, and
+      /// the caller becomes responsible for disposing the target.
+      /// </summary>
+      /// <param name="item">The item to be queued/dequeued for 
+      /// automatic disposal</param>
+      /// <param name="add">true to queue the item for disposal,
+      /// or false to dequeue the item</param>
+      /// <exception cref="ArgumentNullException"></exception>
+
+      public static T AutoDispose<T>(this T item, bool add = true)
+         where T : IDisposable
+      {
+         if(item == null)
+            throw new ArgumentNullException(nameof(item));
+         if(add)
+            Add(item);
+         else
+            Remove(item);
+         return item;
+      }
+   }
+}
+
---
+++ OrderedSet.cs
@@ -0,0 +1,247 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace System.Collections.Generic
+{
+   /// <summary>
+   /// Marshals both a List<T> and a HashSet<T> as a quick/dirty
+   /// means of implementing an ordered set (whose elements are
+   /// ordered like a List, but has fast membership lookup like
+   /// a HashSet). It uses the list to maintain ordering and the
+   /// HashSet to perform membership testing.
+   /// 
+   /// Obviously, this approach has the downside of having to 
+   /// store each element in duplicate, meaning that will use
+   /// more than double the memory as a simple List<T>.
+   /// 
+   /// TODO: Fix issue with equality comparer discrepancy as
+   /// was done in OrderedSet<T> below.
+   /// </summary>
+   /// <typeparam name="T">The element type</typeparam>
+
+   public class HashList<T> : IList<T>
+   {
+      HashSet<T> set;
+      List<T> list;
+      IEqualityComparer<T> comparer;
+
+      public HashList(IEqualityComparer<T> comparer = null)
+      {
+         set = new HashSet<T>(comparer);
+         list = new List<T>();
+         this.comparer = comparer ?? set.Comparer;
+      }
+
+      public HashList(IEnumerable<T> items, IEqualityComparer<T> comparer)
+      {
+         /// TODO: complicated.
+      }
+
+      /// <summary>
+      /// Setter: The given value must not exist at a different index.
+      /// </summary>
+      /// <param name="index"></param>
+      /// <returns></returns>
+      /// <exception cref="ArgumentException"></exception>
+
+      public T this[int index]
+      {
+         get { return list[index]; }
+         set
+         {
+            T item = list[index];
+            if(set.Contains(value))
+            {
+               if(!comparer.Equals(value, item))
+                  throw new ArgumentException("value already exists at different position");
+               set.Remove(item);
+            }
+            list[index] = value;
+            set.Add(value);
+         }
+      }
+
+      public int Count => set.Count;
+
+      public bool IsReadOnly => false;
+
+      public void Add(T item)
+      {
+         if(set.Add(item))
+            list.Add(item);
+      }
+
+      public void Clear()
+      {
+         set.Clear();
+         list.Clear();
+      }
+
+      public bool Contains(T item)
+      {
+         return set.Contains(item);
+      }
+
+      public void CopyTo(T[] array, int arrayIndex)
+      {
+         list.CopyTo(array, arrayIndex);
+      }
+
+      /// <summary>
+      /// Because a user-specified IEqualityComparer<T> is
+      /// supported, we cannot rely on comparisons done by
+      /// the List<T>, which uses the default comparer for
+      /// the element type.
+      /// </summary>
+
+      public int IndexOf(T item)
+      {
+         int cnt = list.Count;
+         for(int i = 0; i < cnt; i++)
+         {
+            if(comparer.Equals(list[i], item))
+               return i;
+         }
+         return -1;
+      }
+
+      public void Insert(int index, T item)
+      {
+         if(!set.Contains(item))
+         {
+            list.Insert(index, item);
+            set.Add(item);
+         }
+         else
+            throw new InvalidOperationException("item exists");
+      }
+
+      public bool Remove(T item)
+      {
+         if(set.Remove(item))
+         {
+            list.RemoveAt(IndexOf(item));
+            return true;
+         }
+         return false;
+      }
+
+      public void RemoveAt(int index)
+      {
+         T item = list[index];
+         list.RemoveAt(index);
+         set.Remove(item);
+      }
+
+      public IEnumerator<T> GetEnumerator()
+      {
+         return list.GetEnumerator();
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return this.GetEnumerator();
+      }
+   }
+
+   /// <summary>
+   /// Marshals both a List<T> and a HashSet<T> as a quick/dirty
+   /// means of implementing an ordered set (whose elements are
+   /// ordered like a List, but have fast membership lookup like
+   /// a HashSet). It uses the list to maintain ordering and the
+   /// HashSet to perform membership testing.
+   /// 
+   /// Obviously, this approach has the downside of having to 
+   /// store each element in duplicate, meaning that it will use 
+   /// slightly more than 2x the memory used by a simple List<T>.
+   /// </summary>
+   /// <typeparam name="T">The element type</typeparam>
+
+   public class OrderedSet<T> : ICollection<T>
+   {
+      HashSet<T> set;
+      List<T> list;
+      IEqualityComparer<T> comparer;
+
+      public OrderedSet(IEqualityComparer<T> comparer = null)
+      {
+         set = new HashSet<T>(comparer);
+         list = new List<T>();
+         this.comparer = comparer;
+      }
+
+      public T this[int index]
+      {
+         get { return list[index]; }
+      }
+
+      public int Count => set.Count;
+
+      public bool IsReadOnly => false;
+
+      public void Add(T item)
+      {
+         if(set.Add(item))
+            list.Add(item);
+      }
+
+      public void Clear()
+      {
+         set.Clear();
+         list.Clear();
+      }
+
+      public bool Contains(T item)
+      {
+         return set.Contains(item);
+      }
+
+      public void CopyTo(T[] array, int arrayIndex)
+      {
+         list.CopyTo(array, arrayIndex);
+      }
+
+      public bool Remove(T item)
+      {
+         if(set.Remove(item))
+         {
+            int pos = IndexOf(item);
+            if(pos > -1)
+               list.RemoveAt(pos);
+            return true;
+         }
+         return false;
+      }
+
+      /// <summary>
+      /// Because a user-specified IEqualityComparer<T> is
+      /// supported, we cannot rely on comparisons done by
+      /// the List<T>, which uses the default comparer for
+      /// the element type.
+      /// </summary>
+
+      public int IndexOf(T item)
+      {
+         if(comparer == null)
+            return list.IndexOf(item);
+         int cnt = list.Count;
+         for(int i = 0; i < cnt; i++)
+         {
+            if(comparer.Equals(list[i], item))
+               return i;
+         }
+         return -1;
+      }
+
+      public IEnumerator<T> GetEnumerator()
+      {
+         return list.GetEnumerator();
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return this.GetEnumerator();
+      }
+   }
+
+
+}
